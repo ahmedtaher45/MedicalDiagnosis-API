849e5b1cae2f0ebd5cb9b82a3b19b8bc046a3e2c
Ahmed Taher <90255766+ahmedtaher45@users.noreply.github.com>
2025-12-07 23:40:16 +0200

    Add Unit of work & repository pattern

---
+++ Diagnosis.Application/Interfaces/IAuth.cs
@@ -0,0 +1,14 @@
+﻿using Diagnosis.Domain.Models.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagnosis.Application.Interfaces
+{
+    public interface IAuth
+    {
+        Task<string> RegisterAsync(ApplicationUser user);
+    }
+}
---
+++ Diagnosis.Infrastracture/Repositories/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using Diagnosis.Application.Interfaces;
+using Diagnosis.Domain.Models.Entites;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diagnosis.Infrastracture.Repositories
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ApplicationDbContext _context;
+
+        public UnitOfWork(UserManager<ApplicationUser> userManager, ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _context = context;
+            Auth = new AuthRepository();
+        }
+
+        public IAuth Auth { get; private set; }
+
+        public Task<int> CompleteAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SaveChangesAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
